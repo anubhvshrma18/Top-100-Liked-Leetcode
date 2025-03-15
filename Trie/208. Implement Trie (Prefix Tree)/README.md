e2b9a3193861634dcd51a2fe1d50003d590033a6
anubhvshrma18 <57109153+anubhvshrma18@users.noreply.github.com>
2025-03-16 00:48:49 +0530

    Trie

---
+++ Trie/208. Implement Trie (Prefix Tree)/README.md
@@ -0,0 +1,12 @@
+# 208. Implement Trie (Prefix Tree)
+
+Leetcode Problem Link - [here](https://leetcode.com/problems/implement-trie-prefix-tree/description/?envType=study-plan-v2&envId=top-100-liked)!
+
+Youtube Video - 
+
+### [Solution]():
+
+```cpp
+
+
+```
---
+++ Trie/208. Implement Trie (Prefix Tree)/solution.cpp
@@ -0,0 +1,58 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = nullptr;
+        }
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int index = c - 'a';
+            if (node->children[index] == nullptr) {
+                node->children[index] = new TrieNode();
+            }
+            node = node->children[index];
+        }
+        node->isEndOfWord = true;
+    }
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int index = c - 'a';
+            if (node->children[index] == nullptr) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node->isEndOfWord;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char c : prefix) {
+            int index = c - 'a';
+            if (node->children[index] == nullptr) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return true;
+    }
+};
